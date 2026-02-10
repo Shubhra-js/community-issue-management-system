a53a5010687e18af68c8fd409b2d340c4d9be69b
Shubhra Jyotsna <shubhrajyotsna@Shubhras-MacBook-Air.local>
2026-02-10 10:48:28 +0530

    Initial Django project setup

---
+++ issues/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Category, Complaint
+
+admin.site.register(Category)
+admin.site.register(Complaint)
+# Register your models here.
---
+++ issues/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+class Complaint(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('In Progress', 'In Progress'),
+        ('Resolved', 'Resolved'),
+    ]
+
+    citizen = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Complaint {self.id} - {self.status}"
