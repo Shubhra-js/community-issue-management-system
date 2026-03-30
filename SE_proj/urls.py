10c6c0fc4bce1fa7628635a10ec10456c6e48b3c
Shubhra-js <shubhrajyotsna06@gmail.com>
2026-03-30 19:47:38 +0530

    Added UI improvements and complaint tracking features

--- SE_proj/urls.py
+++ SE_proj/urls.py
@@ -19,5 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # 👇 ADD THIS LINE
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('issues.urls')),
 ]
